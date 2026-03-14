6549e6d1ba145270ed02c72b88cdf123b7081781
Tejas1551 <994806404@nsd42.net>
2026-03-14 09:23:00 -0400

    Tilt rear cameras back

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -225,17 +225,17 @@ public class Vision {
     /** Rear Left Camera. */
     LEFT_CAM(
         "Arducam_OV9281_Left",
-        new Rotation3d(0, Math.toRadians(-20), Math.toRadians(160)),
+        new Rotation3d(0, Math.toRadians(-25), Math.toRadians(160)),
         new Translation3d(
-            Units.inchesToMeters(-10.0), Units.inchesToMeters(13.0), Units.inchesToMeters(7.9)),
+            Units.inchesToMeters(-8.5), Units.inchesToMeters(13.0), Units.inchesToMeters(7.9)),
         VecBuilder.fill(4, 4, 8),
         VecBuilder.fill(0.5, 0.5, 1)),
     /** Rear Right Camera. */
     RIGHT_CAM(
         "Arducam_OV9281_Right",
-        new Rotation3d(0, Math.toRadians(-20), Math.toRadians(-160)),
+        new Rotation3d(0, Math.toRadians(-25), Math.toRadians(-160)),
         new Translation3d(
-            Units.inchesToMeters(-10.0), Units.inchesToMeters(-13.0), Units.inchesToMeters(7.9)),
+            Units.inchesToMeters(-8.5), Units.inchesToMeters(-13.0), Units.inchesToMeters(7.9)),
         VecBuilder.fill(4, 4, 8),
         VecBuilder.fill(0.5, 0.5, 1)),
 
